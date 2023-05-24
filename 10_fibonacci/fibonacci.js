b0531587b694f4d02d42699cda39b70073e4d6e2
Andrew <andrewmoy3@gmail.com>
2023-05-24 13:09:59 -0400

    finished 10, 11, 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(typeof n === 'string'){
+        n = n.toString();
+    }
+    if(n < 0){
+        return "OOPS";
+    }
+    if(n == 1){
+        return 1
+    }
+    if(n == 2){
+        return 1
+    }
+    let arr = [1,1];
+    for(let i=2;i<n;i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n-1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    ages = people.map(function(person){
+        var death = person.yearOfDeath
+        var birth = person.yearOfBirth;
+        return death-birth;
+    })
+    const i = ages.indexOf(Math.max(...ages))
+    return people[i]
 };
 
 // Do not edit below this line
